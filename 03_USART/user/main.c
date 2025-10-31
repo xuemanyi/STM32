8c4d04e8d549cfff9bffc6f3ba3f24a69d66e4bb
xuemanyi <guoshengw24@gmail.com>
2025-10-31 23:23:19 +0800

    feat: add usart communication protocol

---
+++ 03_USART/user/main.c
@@ -0,0 +1,20 @@
+#include "stm32f10x.h"
+
+int main(void)
+{
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+	
+	USART_InitTypeDef USART_InitStruct;
+	
+	USART_InitStruct.USART_BaudRate = 115200;
+	USART_InitStruct.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
+	USART_InitStruct.USART_WordLength = USART_WordLength_8b;
+	USART_InitStruct.USART_Parity = USART_Parity_No;
+	USART_InitStruct.USART_StopBits = USART_StopBits_1;
+	
+	USART_Init(USART1, &USART_InitStruct);
+	
+	while(1)
+	{
+	}
+}
