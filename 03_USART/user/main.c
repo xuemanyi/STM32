1e8d8ffa56d3c37a5b00d1ec0db891a8c7b3e9cf
xuemanyi <guoshengw24@gmail.com>
2025-11-02 17:19:05 +0800

    feat: add send datas by USART(AFIO)

--- 03_USART/user/main.c
+++ 03_USART/user/main.c
@@ -1,9 +1,11 @@
 #include "stm32f10x.h"
 
+void My_USART_SendBytes(USART_TypeDef *UASRTx, uint8_t *pData, uint16_t size);
+
 int main(void)
 {
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
-	
+
 	USART_InitTypeDef USART_InitStruct;
 	
 	USART_InitStruct.USART_BaudRate = 115200;
@@ -11,10 +13,65 @@ int main(void)
 	USART_InitStruct.USART_WordLength = USART_WordLength_8b;
 	USART_InitStruct.USART_Parity = USART_Parity_No;
 	USART_InitStruct.USART_StopBits = USART_StopBits_1;
+
+#if 0	
+	GPIO_InitTypeDef GPIO_InitStruct;
+
+	//tx
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_9;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_10MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStruct);
+	
+	//rx
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_10 ;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IPU;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_10MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStruct);	
+#endif
+
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+	GPIO_PinRemapConfig(GPIO_Remap_USART1, ENABLE);
+	
+	GPIO_InitTypeDef GPIO_InitStruct; 
+	
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_6;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_10MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStruct);
+	
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_7;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IPU;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_10MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStruct);
 	
 	USART_Init(USART1, &USART_InitStruct);
-	
+	USART_Cmd(USART1, ENABLE);
+
+	uint8_t datas[] = {1, 2, 3, 4, 5};
+
+	My_USART_SendBytes(USART1, datas, 5);
+
 	while(1)
 	{
+		
 	}
+} 
+
+void My_USART_SendBytes(USART_TypeDef *USARTx, uint8_t *pData, uint16_t size)
+{
+	uint32_t i = 0;
+	
+	for (i = 0; i < size; i++) {
+		//wait
+		while (USART_GetFlagStatus(USARTx, USART_FLAG_TXE) == RESET);
+		//send
+		USART_SendData(USARTx, pData[i]);
+	}
+	
+	while (USART_GetFlagStatus(USARTx, USART_FLAG_TC) == RESET);
 }
